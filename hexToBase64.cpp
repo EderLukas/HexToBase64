2a2d8b12ab0222153f1c785411194fa3a149711c
Lukas Eder <eder.lukas@bluewin.ch>
2019-01-29 20:55:32 +0100

    Complete overwork, got taught how to shift bits.

--- hexToBase64.cpp
+++ hexToBase64.cpp
@@ -6,61 +6,54 @@
  #include <vector>
  using namespace std;
 
- void askForInput(unsigned char *string_to_convert) {
-     cout << "\nPlease provide your input: ";
-     cin >> string_to_convert;
- }
+void convert(unsigned int &hex, const unsigned int &mask,
+             vector<unsigned int> &converted_hex) {
 
- vector<unsigned char> processOutput(unsigned char *string_to_convert) {
-     // TODO implement convertion
-     vector<unsigned char> result;
-     for (unsigned char i=0; i <= sizeof(string_to_convert); i++) {
-         result.push_back(string_to_convert[i]);
-     }
-     return result;
- }
+    converted_hex.push_back(hex&mask);
+    do {
+        converted_hex.push_back((hex>>=6)&mask);
+    } while (hex<<6 != 0);
+}
 
-void showOutput(vector<unsigned char> &converted_string) {
-    // FIXME is giving the ascii code right now
-    for (unsigned char &elem : converted_string) {
-        printf("%d\n", elem);
+void createBase64(vector<unsigned int> &converted_hex, vector<char> &base_64){
+    char letter;
+    for (unsigned int &element : converted_hex) {
+        if (element <= 25) {
+            letter = element+65;
+            base_64.push_back(letter);
+        } else if (element <= 26 || element >= 51) {
+            letter = element+71;
+            base_64.push_back(letter);
+        } else if(element <= 52 || element >= 61) {
+            letter = element-4;
+            base_64.push_back(letter);
+        } else if (element == 62) {
+            base_64.push_back('+');
+        } else if (element == 63) {
+            base_64.push_back('/');
+        }
     }
 }
 
-void askForAnotherConvertion(int &convert_again) {
-    string prog_rep;
-    cout << "Would you like to do another convertion? (Y/n): ";
-    cin.ignore();
-    getline(cin >> hex, prog_rep);
-
-    while(true) {
-        if (prog_rep == "n") {
-            convert_again = 0;
-            break;
-        } else if (prog_rep == "y" || prog_rep == "Y" || prog_rep.empty()){
-            convert_again = 1;
-            break;
-        }
-        cout << "\nPlease answer with y or n: ";
-        getline(cin >> hex, prog_rep);
+void displayConvertion(vector<char> &base_64) {
+    printf("\nYour convertion is: ");
+    for(char &i : base_64){
+        printf("%c", i);
     }
+    printf("\n\n");
 }
 
  int main () {
-    int convert_again = 0;
-    unsigned char string_to_convert[255];
-    vector<unsigned char> converted_string;
+    unsigned int hex = 0xA00;
+    //76D206B696C6C696E6720796F757220627261696E206C696B65206120706F69736F6e6F7573206DED757368726F6F6D;
+    const unsigned int mask = 0x3F;
+    vector<unsigned int> converted_hex;
+    vector<char> base_64;
 
-    cout << "\n#######################\n"
-         << "Hex to Base64 converter\n"
-         << "#######################\n\n";
+    convert(hex, mask, converted_hex);
+    createBase64(converted_hex, base_64);
+    // erase 0x of hex number
+    base_64.pop_back();
 
-    do {
-        askForInput(string_to_convert);
-        converted_string = processOutput(string_to_convert);
-        showOutput(converted_string);
-        askForAnotherConvertion(convert_again);
-    } while (convert_again);
-
-    cout << "\nbye!" << endl;
+    displayConvertion(base_64);
  }
