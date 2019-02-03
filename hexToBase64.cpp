7d5a5065a8a06c43e5932bbae387cabe3dd43299
Lukas Eder <eder.lukas@bluewin.ch>
2019-02-03 13:27:15 +0100

    figured a way how to take input into an array. Not smooth yet.

--- hexToBase64.cpp
+++ hexToBase64.cpp
@@ -5,35 +5,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <typeinfo>
 using namespace std;
 
-void getUserInput(string &user_input) {
-    printf("\nPlease enter your hex code: ");
-    getline(cin, user_input);
-}
-
-void convertInputToHex (const string &user_input, vector<unsigned int> &hex_cache) {
-    for(const char &pos : user_input) {
-        if (pos >= 48 && pos <= 57) {
-            hex_cache.push_back(pos-'0');
+void getUserInput(vector<unsigned int> &user_input) {
+    printf("\nPlease enter your hex code (finish with '0x3B'): ");
+    unsigned int input_elem = 0;
+    while (1) {
+        scanf("%x", &input_elem);
+        user_input.push_back(input_elem);
+        // TODO delete debug code
+        cout << "input: " << input_elem << endl;
+        if (input_elem == 0x3B) {
+            while((getchar()) != '\n');
+            break;
         }
-        else if (pos == 65 || pos == 97) {
-            hex_cache.push_back(10);
-        } else if (pos == 66|| pos == 98) {
-            hex_cache.push_back(11);
-        } else if (pos == 67|| pos == 99) {
-            hex_cache.push_back(12);
-        } else if (pos == 68|| pos == 100) {
-            hex_cache.push_back(13);
-        } else if (pos == 69|| pos == 101) {
-            hex_cache.push_back(14);
-        } else if (pos == 70|| pos == 102) {
-            hex_cache.push_back(15);
-        }
+        while((getchar()) != '\n');
     }
 }
 
 void invertCache(const vector<unsigned int> &hex_cache, vector<unsigned int> &inverted_hex_cache) {
+    cout << "hello";
     unsigned int size_of_cache = hex_cache.size();
     for (int i = size_of_cache - 1; i >= 0; i--) {
         inverted_hex_cache.push_back(hex_cache.at(i));
@@ -41,17 +33,16 @@ void invertCache(const vector<unsigned int> &hex_cache, vector<unsigned int> &in
 }
 
 void printBase64Char(unsigned int &letter){
-    cout << letter << endl;
     if (letter <= 25) {
-        //printf("%c", letter+65);
+        printf("%c", letter+65);
     } else if (letter >= 26 || letter <= 51) {
-        //printf("%c", letter+71);
+        printf("%c", letter+71);
     } else if(letter >= 52 || letter <= 61) {
-        //printf("%c", letter-4);
+        printf("%c", letter-4);
     } else if (letter == 62) {
-        //cout << '+';
+        cout << '+';
     } else if (letter == 63) {
-        //cout << '/';
+        cout << '/';
     }
 }
 
@@ -60,12 +51,21 @@ void binaryConvertionToBase64(vector<unsigned int> &hex_cache,
                               void printBase64(unsigned int &letter)) {
 
     int cache_size = hex_cache.size();
+    // TODO delete debug code
+    cout << "cache size: " << cache_size << endl;
     if (cache_size % 3 != 0) {
         int delimiter = cache_size % 3;
         for (int i = 0; i < delimiter; i++) {
             hex_cache.insert(hex_cache.begin(), 0);
         }
     }
+
+    // TODO delete debug code
+    cout << "hex_cache: ";
+    for (unsigned int &hex : hex_cache){
+        cout << hex;
+    }
+    cout << endl;
 
     int iterations_left = cache_size;
     int index = 0;
@@ -102,13 +102,9 @@ int main () {
     vector<unsigned int> hex_cache;
     vector<unsigned int> inverted_hex_cache;
     const unsigned int mask = 0x3F;
-    string user_input;
+    vector<unsigned int> user_input;
 
     getUserInput(user_input);
-    convertInputToHex(user_input, hex_cache);
-    //invertCache(hex_cache, inverted_hex_cache);
-    for (unsigned int i : hex_cache) {
-        cout << i;
-    }
-    binaryConvertionToBase64(hex_cache, mask, printBase64Char);
+    invertCache(hex_cache, inverted_hex_cache);
+    binaryConvertionToBase64(inverted_hex_cache, mask, printBase64Char);
 }
